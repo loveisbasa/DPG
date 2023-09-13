b075c08703027e7ecea5278227dd504a319dcca5
Aosong Feng <45991889+asFeng@users.noreply.github.com>
2023-09-12 23:27:44 -0400

    Update dataloader.py

--- data/dataloader.py
+++ data/dataloader.py
@@ -54,6 +54,28 @@ class FFHQDataset(VisionDataset):
 
         return img
 
+    
+@register_dataset(name='celebhq')
+class CelebHQDataset(VisionDataset):
+    def __init__(self, root: str, transforms: Optional[Callable]=None):
+        super().__init__(root, transforms)
+
+        self.fpaths = sorted(glob(root + '/**/*.jpg', recursive=True))
+        assert len(self.fpaths) > 0, "File list is empty. Check the root."
+
+    def __len__(self):
+        return len(self.fpaths)
+
+    def __getitem__(self, index: int):
+        fpath = self.fpaths[index]
+        img = Image.open(fpath).convert('RGB')
+        
+        if self.transforms is not None:
+            img = self.transforms(img)
+        
+        return img
+
+
 @register_dataset(name='imagenet')
 # sorted dataset
 # class ImageNetDataset(VisionDataset):
